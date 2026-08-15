0aa0af8a48d2de53f930be623273ecbce8b5bb2f
abalea <abalea@f2r5s6.paris.42.school>
2026-08-15 18:46:34 +0200

    Made the matrix parser

--- constraint_propag.c
+++ constraint_propag.c
@@ -6,8 +6,33 @@
 /*   By: abalea <abalea@learner.42.tech>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/15 17:50:38 by abalea            #+#    #+#             */
-/*   Updated: 2026/08/15 17:50:39 by abalea           ###   ########.fr       */
+/*   Updated: 2026/08/15 18:46:15 by abalea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+/**int	constraint_propag(*int matrix)
+{
+		
+}
+*/
+
+int	main(int argc, char *argv[])
+{
+	char	table[4][4];
+	int		row;
+	int		col;
+	int		i;
+
+	i = 1;
+	while (i < argc)
+	{
+		row = (i - 1) / 4;
+		col = (i - 1) % 4;
+		table[row][col] = *argv[i];
+		i++;
+	}
+	write(1, &table[2][2], 1);
+	/*constraint_propag(int);*/
+}
