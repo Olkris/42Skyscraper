2a067a1c3342743d3cedfb7bf2b7cf5e06046bf2
Olkris <olkris2666@gmail.com>
2026-08-16 16:35:32 +0200

    test main for checker function

--- checker.c
+++ checker.c
@@ -1,12 +1,21 @@
 
 #include <unistd.lib>
 
-bool	checker(int *matrix[][], int *visibility[])
+bool	checker(int matrix[4][4], int visibility[16])
 {
 	
 }
 
 int main(void)
 {
-	int 
+	int	matrix[4][4] = {
+		{1, 2, 3, 4},
+		{1, 2, 3, 4},
+		{1, 2, 3, 4},
+		{1, 2, 3, 4}
+	};
+
+	int visibility[16] = {1, 2, 3, 4, 4, 3, 2, 1, 1, 2, 3, 4, 4, 3, 2, 1};
+
+	checker(matrix, visibility);
 }
