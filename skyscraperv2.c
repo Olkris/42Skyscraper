dde9e4e19eade914f3feecb2a026252ca0b7e980
iameskou <iameskou@f2r5s6.paris.42.school>
2026-08-16 18:00:37 +0200

    add about need exaclty 16 and between 1 and 4

--- skyscraperv2.c
+++ skyscraperv2.c
@@ -1,27 +1,93 @@
-
 #include <unistd.h>
+#include <stdio.h>
+
+/*
+ * Display an error message.
+ */
+void	print_error(char *message)
+{
+	write(1, message, 40);
+}
+
+/*
+ * Check the input and fill the numbers array.
+ *
+ * The input must contain:
+ * - Numbers from 1 to 4
+ * - Exactly 16 numbers
+ * - Spaces between the numbers
+ */
+int	parse_args(char *arguments, int *numbers)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (arguments[i] != '\0')
+	{
+		/* Check if the character is between 1 and 4 */
+		if (arguments[i] >= '1' && arguments[i] <= '4')
+			count++;
+
+		/* Spaces are allowed */
+		else if (arguments[i] != ' ')
+		{
+			printf("Error: numbers must be between 1 and 4\n");
+			return (1);
+		}
+		i++;
+	}
+
+	/* Check if we have exactly 16 numbers */
+	if (count != 16)
+	{
+		printf("Error: need exactly 16 numbers\n");
+		return (1);
+	}
+
+	/*
+	 * Convert each character from char to int.
+	 * i * 2 skips the spaces.
+	 */
+	i = 0;
+	while (i < 16)
+	{
+		numbers[i] = arguments[i * 2] - '0';
+		i++;
+	}
+
+	return (0);
+}
 
 int	main(int argc, char *argv[])
 {
+	int	numbers[16];
+	int	i;
+
+	/* Check that there is exactly one argument */
 	if (argc != 2)
 	{
-		print_error();
+		printf("Error: enter exactly 16 numbers\n");
 		return (1);
 	}
-	else
+
+	/* Check input and fill the array */
+	if (parse_args(argv[1], numbers) != 0)
+		return (1);
+
+	/* Display the original input */
+	printf("Input: %s\n", argv[1]);
+
+	/* Display all numbers */
+	i = 0;
+	while (i < 16)
 	{
-		parse_args(argv[1])
+		printf("Number %d: %d\n", i, numbers[i]);
+		i++;
 	}
-}
 
-*int	parse_args(char *arguments)
-{
-	
+	return (0);
 }
+// char	print_result(int *result);
 
-char	print_result(int *result);
-
-char	print_error(void)
-{
-	write(1, "Error\n", 6)
-}
