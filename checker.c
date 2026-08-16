8f8e2b528a57824e04e88f582816e54f04c454af
Olkris <olkris2666@gmail.com>
2026-08-16 16:58:44 +0200

    Visible buildings counter

--- checker.c
+++ checker.c
@@ -3,7 +3,38 @@
 
 bool	checker(int matrix[4][4], int visibility[16])
 {
-	
+	int	i = 0;
+	int col;
+	int row;
+
+	while (i < 16)
+	{
+		col = 0;
+		matrix[]
+	}
+}
+
+int	count_visible(int line[4])
+{
+	int i;
+	int height;
+	int max_height;
+	int visible_count;
+
+	i = 0;
+	max_height = 0;
+	visible_count = 0;
+	while (i < 4)
+	{
+		height = line[i];
+		if (height > max_height)
+		{
+			visible_count += 1;
+			max_height = height;
+		}
+		i++;
+	}
+	return (visible_count);
 }
 
 int main(void)
@@ -17,5 +48,5 @@ int main(void)
 
 	int visibility[16] = {1, 2, 3, 4, 4, 3, 2, 1, 1, 2, 3, 4, 4, 3, 2, 1};
 
-	checker(matrix, visibility);
+	vis_checker(matrix, visibility);
 }
